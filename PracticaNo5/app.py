6e4b8af20d8ba4e00b1b6029f5f66c60d29acfd5
myriam120 <4422399895mg@gmail.com>
2023-06-15 18:41:56 -0600

    rutas y variables de conexion

--- PracticaNo5/app.py
+++ PracticaNo5/app.py
@@ -1,11 +1,30 @@
 from flask import Flask
 
+# Inicializacion del servidor Flask 
 app= Flask(__name__)
 
+app.config['MYSQL_HOST']= "localhost"
+app.config['MYSQL_USER']= "root"
+app.config['MYSQL_PASSWORD']= ""
+app.config['MYSQL_DB']= "dbflask"
+
+#Declaracion de ruta 
+
+#ruta Index o ruta principal  http://localhost:5000
+# ruta se compone de nombre y funcion 
 @app.route('/')
 def index():
     return "Hola Mundo"
 
+@app.route('/guardar')
+def guardar():
+    return "Se guardo el album en la BD"
+
+@app.route('/eliminar')
+def eliminar():
+    return "Se elimino"
+
+#Ejecucion
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
     
