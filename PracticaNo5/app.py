61f6fb314c7ce35da7e2eabc1f0468ac3141fc2b
myriam120 <4422399895mg@gmail.com>
2023-06-16 18:49:11 -0600

    practica6: vistas, envio infomación devista a rutas

--- PracticaNo5/app.py
+++ PracticaNo5/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 # Inicializacion del servidor Flask 
 app= Flask(__name__)
 
+#configuracion para la conexion a BD
 app.config['MYSQL_HOST']= "localhost"
 app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= ""
@@ -14,11 +15,18 @@ app.config['MYSQL_DB']= "dbflask"
 # ruta se compone de nombre y funcion 
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return render_template('index.html')
 
-@app.route('/guardar')
+@app.route('/guardar', methods=['POST'])
 def guardar():
-    return "Se guardo el album en la BD"
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print(titulo, artista, anio)
+
+    return "La info del Album llego a su ruta Amigo ;)"
+
 
 @app.route('/eliminar')
 def eliminar():
