373e06d640234ec8b3b8a13b71efe85de136bdb3
myriam120 <4422399895mg@gmail.com>
2023-06-20 18:50:25 -0600

    insert a la BD, redirect y mensajes a la vista

--- PracticaNo5/app.py
+++ PracticaNo5/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
 
 # Inicializacion del servidor Flask 
 app= Flask(__name__)
@@ -9,6 +11,9 @@ app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= ""
 app.config['MYSQL_DB']= "dbflask"
 
+app.secret_key='mysecretkey'
+
+mysql= MySQL(app)
 #Declaracion de ruta 
 
 #ruta Index o ruta principal  http://localhost:5000
@@ -20,12 +25,16 @@ def index():
 @app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        print(titulo, artista, anio)
-
-    return "La info del Album llego a su ruta Amigo ;)"
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+        #print(titulo, artista, anio)
+        cs = mysql.connection.cursor()
+        cs.execute('insert into albums (titulo, artista, anio) values(%s,%s,%s)', (Vtitulo,Vartista, Vanio))    
+        mysql.connection.commit()
+        
+    flash('Album agregado correctamente :)')
+    return redirect(url_for('index'))
 
 
 @app.route('/eliminar')
