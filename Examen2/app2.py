9b52f1e640efdbbd89399b9e0f5a867ff404c2b0
myriam120 <4422399895mg@gmail.com>
2023-07-20 18:54:21 -0600

    conocer platilla, barra de navegacion y boostrap

--- Examen2/app2.py
+++ Examen2/app2.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
-# Inicialización del servidor Flask
 app = Flask(__name__)
 
-# Configuración para la conexión a BD
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
@@ -28,21 +26,20 @@ def guardar():
         VNombre = request.form['txtNombre']
         VCantidad = request.form['txtCantidad']
         VPrecio = request.form['txtPrecio']
-        #print(titulo, artista, anio)
+
         cs = mysql.connection.cursor()
-        cs.execute('insert into dbflores (nombre, cantidad, precio) values(%s,%s,%s)', (VNombre,VCantidad,VPrecio)); 
+        cs.execute('INSERT INTO dbflores (nombre, cantidad, precio) VALUES (%s, %s, %s)', (VNombre, VCantidad, VPrecio))
         mysql.connection.commit()
-        
+
     flash('La flor fue guardada correctamente :)')
     return redirect(url_for('index2'))
-
 
 @app.route('/tabla')
 def tabla():
     curEditar = mysql.connection.cursor()
     curEditar.execute('SELECT * FROM dbflores')
     consulta = curEditar.fetchall()
-    return render_template('tablaflores.html', flores=consulta)
+    return render_template('tablaf.html', flores=consulta)
 
 @app.route('/editar/<id>', methods=['GET', 'POST'])
 def editar(id):
@@ -50,18 +47,34 @@ def editar(id):
     curEditar.execute('SELECT * FROM dbflores WHERE id = %s', (id,))
     consulta = curEditar.fetchone()
 
+    # Actualizar la flor
+    if request.method == 'POST':
+        nuevoNombre = request.form['txtNombre']
+        nuevaCantidad = request.form['txtCantidad']
+        nuevoPrecio = request.form['txtPrecio']
+
+        curActualizar = mysql.connection.cursor()
+        curActualizar.execute('UPDATE dbflores SET nombre = %s, cantidad = %s, precio = %s WHERE id = %s', (nuevoNombre, nuevaCantidad, nuevoPrecio, id))
+        mysql.connection.commit()
+
+        flash('La flor ha sido actualizada correctamente :)')
+        return redirect(url_for('index2'))
+
+    return render_template('editar2.html', flor=consulta)
+
 @app.route('/buscar', methods=['GET', 'POST'])
 def buscar():
     if request.method == 'POST':
         nombre = request.form['txtNombre']
         curBuscar = mysql.connection.cursor()
-        curBuscar.execute('SELECT * FROM dbflores WHERE fruta LIKE %s', (f'%{nombre}%',))
+        curBuscar.execute('SELECT * FROM dbflores WHERE nombre LIKE %s', (f'%{nombre}%',))
         consulta = curBuscar.fetchall()
-        return render_template('buscarflor.html', frutas=consulta)
+        return render_template('buscarflor.html', flores=consulta)
 
     return render_template('buscarflor.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
 
 
